d002d3d2fd74f7c73c83d420f53ba4b97a9be00c
sufyanali07 <sufyan.ali6800@gmail.com>
2022-12-05 02:18:39 +0500

    Files Added

---
+++ public/app.js
@@ -0,0 +1,30 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//===============IMAGE SLIDER=======================
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("Slider");
+  let dots = document.getElementsByClassName("bullets");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+//==================================================
